c0ff625f2d170a9b6f1224bdb5b968c180befcba
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2022-08-09 00:08:04 -0300

    bug fixes e relacionamentos has_many sell_products e sell_services

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,8 +1,18 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
   enum status: { finished: 0, canceled: 1 }
-
   validates :client, presence: true
+
+  belongs_to :discount
+  belongs_to :client
+
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services
+
+  before_save :set_total
 
   def fae_display_field
     id
@@ -12,6 +22,16 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
+  private
+  def set_total
+    total = 0
+    self.products.each {|p| total += p.price}
+    self.services.each {|s| total += s.price}
+
+    if self.discount.present?
+      total -= self.discount.value
+    end
+    total = (total >= 0) ? total : 0
+    self.total = total
+  end
 end
